4e713425fd593d891de26fd5b470bf783cfd7a14
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-02-25 14:05:01 -0800

    got up to countMaker

--- index.js
+++ index.js
@@ -181,8 +181,12 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  const runnerNames = [];
+  runners.forEach(function(item, i){
+    runnerNames[i] = `${runners[i].last_name}, ${runners[i].first_name}`;
+  }); 
+   return runnerNames;
 }
 
 /**
@@ -197,8 +201,11 @@ function getFullNames(/* CODE HERE */) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  const firstCaps = runners.map(function(item){
+    return item.first_name.toUpperCase();
+  })
+  return firstCaps;
 }
 
 /**
@@ -214,8 +221,11 @@ function firstNamesAllCaps(/* CODE HERE */) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+  const shirtFilter = runners.filter(function(item){
+    return item.shirt_size === tShirtSize;
+  })
+  return shirtFilter;
 }
 
 /**
@@ -228,8 +238,11 @@ function getRunnersByTShirtSize(/* CODE HERE */) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+function tallyUpDonations(runners) {
+  function sum(total, num){
+    return total + num.donation;
+  }
+ return runners.reduce(sum,0);
 }
 
 /////////////// CLOSURES ///////////////
@@ -250,8 +263,8 @@ function tallyUpDonations(/* CODE HERE */) {
 */
 function counterMaker() {
   // BROKEN CODE STARTS
-  const count = 0;
-  function counter() {
+  let count = 0;
+  return function counter() {
     ++count
   }
   // BROKEN CODE ENDS
